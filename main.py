ca8d9421df9807f84b196f9424cffec45176952a
ksg-dev <ksg.dev.data@gmail.com>
2024-07-23 12:46:50 -0500

    generate post previews on home

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import requests
+
+
+
+blog_url = "https://api.npoint.io/5910111f7431b1d7e52d"
+all_posts = requests.get(blog_url).json()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/about")
